f8452d96a3a269eec68b9e2fb63c48db9b4e4923
mac <mac@macde-MacBook-Pro.local>
2017-10-06 13:54:55 +0800

    first stage is ok

---
+++ 生命靈數進階版/DatePickerViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DatePickerViewController.swift
+//  生命靈數進階版
+//
+//  Created by mac on 2017/10/6.
+//  Copyright © 2017年 justwithin. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+  
+    @IBOutlet weak var txtDatePicker: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showDatePicker()
+        
+    }
+    
+    
+    func showDatePicker(){
+        //Formate Date
+        datePicker.datePickerMode = .date
+        
+        //ToolBar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.bordered, target: self, action: "donedatePicker")
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.bordered, target: self, action: "cancelDatePicker")
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        
+        txtDatePicker.inputAccessoryView = toolbar
+        txtDatePicker.inputView = datePicker
+        
+    }
+    
+    func donedatePicker(){
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        txtDatePicker.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    func cancelDatePicker(){
+        self.view.endEditing(true)
+    }
+    
+}
